21c6101f42eee3478e4d20a9976f5702c7ce4ed1
Alex2804 <almu.hp@arcor.de>
2019-08-04 18:06:17 +0200

    fixed missing -Iinclude flag and file extensions in compile_libtcc1.sh

--- tcc/compile_libtcc1.sh
+++ tcc/compile_libtcc1.sh
@@ -28,22 +28,19 @@ if [ $TCC_ARCHITECTURE = x86_64 ]; then
   ./$TCC -c lib_libtcc1/va_list.c
   ./$TCC -c lib_libtcc1/bcheck.c -Iinclude
   ./$TCC -ar rcs lib/libtcc1-64.a libtcc1.o va_list.o bcheck.o alloca86_64.o alloca86_64-bt.o
-elif [ $TCC_ARCHITECTURE = i386 ]
-then
+elif [ $TCC_ARCHITECTURE = i386 ]; then
   ./$TCC -c lib_libtcc1/libtcc1.c
-  ./$TCC -c lib_libtcc1/alloca86.c
-  ./$TCC -c lib_libtcc1/alloca86-bt.c
-  ./$TCC -c lib_libtcc1/bcheck.c
-  ./$TCC -ar rcs lib/libtcc1-64.a libtcc1.o alloca86.o alloca86-bt.o bcheck.o
-elif [ $TCC_ARCHITECTURE = arm ]
-then
+  ./$TCC -c lib_libtcc1/alloca86.S
+  ./$TCC -c lib_libtcc1/alloca86-bt.S
+  ./$TCC -c lib_libtcc1/bcheck.c -Iinclude
+  ./$TCC -ar rcs lib/libtcc1-32.a libtcc1.o alloca86.o alloca86-bt.o bcheck.o
+elif [ $TCC_ARCHITECTURE = arm ]; then
   ./$TCC -c lib_libtcc1/libtcc1.c
   ./$TCC -c lib_libtcc1/armeabi.c
-  ./$TCC -c lib_libtcc1/alloca-arm.c
+  ./$TCC -c lib_libtcc1/alloca-arm.S
   ./$TCC -c lib_libtcc1/armflush.c
-  ./$TCC -ar rcs libtcc1.o armeabi.o alloca-arm.o armflush.o
-elif [ $TCC_ARCHITECTURE = arm64 ]
-then
+  ./$TCC -ar rcs lib/libtcc1-arm.a libtcc1.o armeabi.o alloca-arm.o armflush.o
+elif [ $TCC_ARCHITECTURE = arm64 ]; then
   ./$TCC -c lib_libtcc1/lib-arm64.c
   ./$TCC -ar rcs lib/libtcc1-arm64.a lib-arm64.o
 else
